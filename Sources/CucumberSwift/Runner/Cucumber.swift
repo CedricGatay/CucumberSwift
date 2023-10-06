b6f94c5c9a8bcb9df1ff99b9d286507898c4111e
Cedric Gatay <c.gatay@code-troopers.com>
2023-10-06 10:13:36 +0200

    chore(): xcode 15 fix
    For some reason XCTestObservation extension is not taken into account under XCode 15

--- Sources/CucumberSwift/Runner/Cucumber.swift
+++ Sources/CucumberSwift/Runner/Cucumber.swift
@@ -10,7 +10,7 @@ import Foundation
 import XCTest
 import CucumberSwiftExpressions
 
-@objc public class Cucumber: NSObject { // swiftlint:disable:this type_body_length
+@objc public class Cucumber: NSObject, XCTestObservation { // swiftlint:disable:this type_body_length
     static var shared = Cucumber()
 
     var features = [Feature]()
@@ -284,5 +284,34 @@ import CucumberSwiftExpressions
                              file: file,
                              executeSelector: selector,
                              executeClass: `class`)
+    }
+    
+    public func testBundleDidFinish(_ testBundle: Bundle) {
+        reporters.forEach { $0.testSuiteFinished(at: Date()) }
+        let name = Cucumber.shared.reportName.appending(String(testBundle.bundleURL.lastPathComponent.prefix { $0 != "." })).appending(".json")
+        if  let documentDirectory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false),
+            let reportURL = Reporter.reportURL {
+            let fileURL = documentDirectory.appendingPathComponent(name)
+            try? FileManager.default.copyItem(at: reportURL, to: fileURL)
+        }
+    }
+
+    public func testCase(_ testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: Int) {
+        Cucumber.shared.currentStep?.result = .failed(description)
+        Cucumber.shared.currentStep?.errorMessage = description
+        Cucumber.shared.currentStep?.endTime = Date()
+        guard let step = Cucumber.shared.currentStep,
+              let scenario = step.scenario else {
+            return
+        }
+        Cucumber.shared.failedScenarios.append(scenario)
+        var foundStep = false
+        scenario.steps.forEach { step in
+            if step === Cucumber.shared.currentStep {
+                foundStep = true
+            } else if foundStep && step.result == .pending {
+                step.result = .skipped
+            }
+        }
     }
 }
